f87abbd4f9a7bc361ef6cf86521655645bd2e690
Konstantin Petrov <77064122+konstantin-p-petrov@users.noreply.github.com>
2021-01-25 13:52:51 +0200

    Update MainController.java

--- my-app/src/main/java/com/mkyong/core/controller/MainController.java
+++ my-app/src/main/java/com/mkyong/core/controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.mkyong.core.services.HelloServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
     
 @Controller
 public class MainController {
