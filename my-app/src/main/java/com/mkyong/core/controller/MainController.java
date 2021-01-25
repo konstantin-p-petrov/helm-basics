f6fec3c5077f26a75efc6a19d9c3242cfb5fc485
Konstantin Petrov <77064122+konstantin-p-petrov@users.noreply.github.com>
2021-01-25 13:17:51 +0200

    Update MainController.java

--- my-app/src/main/java/com/mkyong/core/controller/MainController.java
+++ my-app/src/main/java/com/mkyong/core/controller/MainController.java
@@ -3,14 +3,15 @@ package com.mkyong.core.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import com.mkyong.core.services.HelloServiceImpl;
+    
 @Controller
 public class MainController {
 
     @ResponseBody
     @GetMapping("/")
     public String hello() {
-        return "Hello Jenkins";
+        return HelloServiceImpl.get();
     }
 
 }
