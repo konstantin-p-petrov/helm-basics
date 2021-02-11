d2585cf0a07c93a3789fd9c79f8f8cf37be68358
Konstantin Petrov <konstantin.ppetrov@scalefocus.com>
2021-02-11 15:01:47 +0200

    changed tests

--- my-app/src/test/java/com/mkyong/core/services/HelloServiceTest.java
+++ my-app/src/test/java/com/mkyong/core/services/HelloServiceTest.java
@@ -17,7 +17,7 @@ public class HelloServiceTest {
     @DisplayName("Test Spring @Autowired Integration")
     @Test
     void testGet() {
-        assertEquals("Hello Jenkins", helloService.get());
+        assertEquals("Hello Koce", helloService.get());
     }
 
 }
